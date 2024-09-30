9212c370e3932bf8f5c11e01a3d5d56bad4381d2
George Dornan (50076129) <50076129@belfastmet.ac.uk>
2024-09-30 15:01:55 +0100

    Initial Commit

---
+++ core/recipe/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.recipes, name='recipe_list'),
+    path('update_recipe/<int:id>/', views.update_recipe, name='update_recipe'),
+    path('delete_recipe/<int:id>/', views.delete_recipe, name='delete_recipe'),
+    path('api', views.api_v1, name='api_v1'),
+]
